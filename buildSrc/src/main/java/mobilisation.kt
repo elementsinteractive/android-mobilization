afa9bbb3574f394d65a917a0842ed3aae3801b7b
Jeffrey de Looff <jeffrey.delooff@elements.nl>
2020-06-23 16:32:05 +0200

    Removes unused imports

--- buildSrc/src/main/java/mobilisation.kt
+++ buildSrc/src/main/java/mobilisation.kt
@@ -1,6 +1,3 @@
-import Mobilisation.Artifacts.loggingApi
-import org.gradle.api.Project
-
 object Mobilisation {
     const val githubUrl = "https://maven.pkg.github.com/elementsinteractive/android-mobilisation"
     const val version = "0.1.0"
