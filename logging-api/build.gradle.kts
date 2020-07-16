e5a281c9333aeaac59130cca1305a911f3c2e5c5
Jeffrey de Looff <jeffrey.delooff@elements.nl>
2020-07-16 13:10:29 +0200

    Updates signing set-up

--- logging-api/build.gradle.kts
+++ logging-api/build.gradle.kts
@@ -16,29 +16,3 @@ dependencies {
     testImplementation(Libs.Kotlin.test)
     testImplementation(Libs.Kotlin.testJunit)
 }
-
-publishing {
-    publications {
-        create<MavenPublication>("gpr") {
-            run {
-                groupId = Mobilization.groupId
-                artifactId = Mobilization.Artifacts.loggingApi
-                version = Mobilization.version
-
-                from(components["java"])
-            }
-        }
-    }
-
-    repositories {
-        maven {
-            name = "GitHubPackages"
-            url = uri(Mobilization.githubUrl)
-
-            credentials {
-                username = System.getenv("GITHUB_ACTOR")
-                password = System.getenv("GITHUB_TOKEN")
-            }
-        }
-    }
-}
