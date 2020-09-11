55fb8d2762713ed54e46088171c32436f87c3fc8
Jeffrey de Looff <jeffrey.delooff@elements.nl>
2020-09-11 16:19:32 +0200

    Updates order of gradle.properties

--- release/signing.sh
+++ release/signing.sh
@@ -10,11 +10,10 @@ create_gradle_properties() {
   mkdir -p ~/.gradle
 
   rm -f $GPG_LOCATION
-  rm -f $GRADLE_PROPERTIES_LOCATION
 
   echo $GPG_KEY_CONTENTS | base64 -d > $GPG_LOCATION
 
-  printf "signing.keyId=$KEYID\nsigning.password=$PASSWORD\nsigning.secretKeyRingFile=$GPG_LOCATION\n" >> $GRADLE_PROPERTIES_LOCATION
+  printf "\nsigning.keyId=$KEYID\nsigning.password=$PASSWORD\nsigning.secretKeyRingFile=$GPG_LOCATION\n" >> $GRADLE_PROPERTIES_LOCATION
 }
 
 create_gradle_properties $ORG_GRADLE_PROJECT_SIGNINGKEYID $ORG_GRADLE_PROJECT_SIGNINGPASSWORD $GPG_KEY_CONTENTS
