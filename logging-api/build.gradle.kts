ae4c2902223a2472a6c83f0ee9310950b7ec61ac
Jeffrey de Looff <jeffrey.delooff@elements.nl>
2020-12-14 10:51:26 +0100

    Adds apply java to pure kotlin modules

--- logging-api/build.gradle.kts
+++ logging-api/build.gradle.kts
@@ -1,7 +1,13 @@
 plugins {
+    id("java-library")
     id("kotlin")
     kotlin("kapt")
     id("com.vanniktech.maven.publish")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 dependencies {
